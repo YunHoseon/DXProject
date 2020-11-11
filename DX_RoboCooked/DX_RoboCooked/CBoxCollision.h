54faf5a5dbd5342c0ac660291f95ab15ee61df3b
jaewook <3355moijam@gmail.com>
2020-11-11 17:40:25 +0900

    콜라이더 제작 중

---
+++ DX_RoboCooked/DX_RoboCooked/CBoxCollision.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ICollisionArea.h"
+
+class CBoxCollision : public ICollisionArea
+{
+public:
+	CBoxCollision(D3DXVECTOR3 vHalfSize);
+	~CBoxCollision() override;
+	bool Render() override;
+protected:
+	bool CollideToBox(ICollisionArea* pTargetCollider) override;
+	bool CollideToSphere(ICollisionArea* pTargetCollider) override;
+public:
+	
+};
