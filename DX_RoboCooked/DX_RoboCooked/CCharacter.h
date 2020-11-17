4eb61b6e7be331327e4b0d0ce49519ebce86a454
MyeungJunLee <mong356@naver.com>
2020-11-17 17:40:35 +0900

    수정

--- DX_RoboCooked/DX_RoboCooked/CCharacter.h
+++ DX_RoboCooked/DX_RoboCooked/CCharacter.h
@@ -2,6 +2,7 @@
 #include "CGameScene.h"
 class CGameScene;
 
+
 enum class ePlayerState
 {
 	E_None, //기본상태
@@ -12,12 +13,20 @@ class CCharacter
 {
 protected:
 	CGameScene*			m_GameCenter;
-	ICollisionArea*		m_pInteractCollision;
 	ePlayerState		m_ePlayerState;
+	ICollisionArea*		m_pInteractCollision;
+	float				m_fSpeed;
+	float				m_fRotY;
+	D3DXVECTOR3			m_vDirection;
+	D3DXVECTOR3			m_vPosition;
+	
 public:
 	CCharacter();
-	~CCharacter();
-
+	virtual ~CCharacter() = 0;
+	virtual void Render() = 0;
+	virtual void Update() = 0;
+	
+	D3DXVECTOR3& GetPosition() { return m_vPosition; }
 	ICollisionArea* GetInteractCollsion() { return m_pInteractCollision; }
 };
 
