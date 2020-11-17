c19ddaad212fd910afd3eb031f981d24454b58c6
MyeungJunLee <mong356@naver.com>
2020-11-17 19:43:49 +0900

    수정 완

--- DX_RoboCooked/DX_RoboCooked/CCharacter.cpp
+++ DX_RoboCooked/DX_RoboCooked/CCharacter.cpp
@@ -6,7 +6,10 @@ CCharacter::CCharacter()
 	: m_GameCenter(NULL)
 	, m_pInteractCollision(nullptr)
 	, m_ePlayerState(ePlayerState::E_None)
+	, m_fSpeed(0.2f)
+	, m_fRotY(0)
 {
+	m_vPosition = D3DXVECTOR3(0,0,0);
 }
 
 CCharacter::~CCharacter()
