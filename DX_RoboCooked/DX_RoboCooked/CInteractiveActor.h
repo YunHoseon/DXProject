dc6f9b7903ada8d7cd3304ce42b7ba1fec22841f
MyeungJunLee <mong356@naver.com>
2020-11-18 15:26:11 +0900

    조합기 시작대기중

--- DX_RoboCooked/DX_RoboCooked/CInteractiveActor.h
+++ DX_RoboCooked/DX_RoboCooked/CInteractiveActor.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "CActor.h"
+
 class CCharacter;
+
 
 class CInteractiveActor :
 	public CActor
---
+++ DX_RoboCooked/DX_RoboCooked/CPartCombinator.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CInteractiveActor.h"
+class CPartCombinator :
+	public CInteractiveActor
+{
+public:
+	CPartCombinator();
+	~CPartCombinator();
+
+	
+};
+
