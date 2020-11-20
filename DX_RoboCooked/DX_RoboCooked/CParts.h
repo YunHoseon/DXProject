1d7832d4345e2c3eb2e6c288cabdac2bc731bd6b
YunHoseon <hosun2776@gmail.com>
2020-11-20 17:35:49 +0900

    파츠 파싱 진행중

--- DX_RoboCooked/DX_RoboCooked/CParts.h
+++ DX_RoboCooked/DX_RoboCooked/CParts.h
@@ -17,6 +17,7 @@ private:
 	int					m_nPartsID;
 	float				m_fRotAngle;
 	float				m_fWeight;
+	D3DXVECTOR3			m_vSize;
 
 public:
 	CParts(int nPartsID);
@@ -25,16 +26,31 @@ public:
 	void Setup();
 	void Update() override;
 	void Render() override;
+	
 	void OnEvent(EEvent eEvent, void* _value);
 	void Interact(CCharacter* pCharacter) override;
-	void SetPosition(D3DXVECTOR3 vPosition) { m_vPosition = vPosition; }
-	void SetGrabPosition(D3DXVECTOR3* vPosition) { m_vGrabPosition = vPosition; }
-	D3DXVECTOR3* GetGrabPosition() { return m_vGrabPosition; }
+	
 	void DownParts(D3DXVECTOR3 vDir);
 	void PartsRotate();
-	int GetPartsID() { return m_nPartsID; }
 	void MoveParts();
 	void SetCombinatorPosition(D3DXVECTOR3 vPosition){ m_vCombinatorPosition = vPosition; }
+
+	/* getter setter */
+	
+	int GetPartsID() { return m_nPartsID; }
+	void SetPartsID(int	ID) { m_nPartsID = ID; }
+
+	float GetWeight() { return m_fWeight; }
+	void SetWeight(float weight) { m_fWeight = weight; }
+
+	D3DXVECTOR3 GetSize() { return m_vSize; }
+	void SetSize(D3DXVECTOR3 size) { m_vSize = size; }
+	
+	void SetPosition(D3DXVECTOR3 vPosition) { m_vPosition = vPosition; }
+	
+	D3DXVECTOR3* GetGrabPosition() { return m_vGrabPosition; }
+	void SetGrabPosition(D3DXVECTOR3* vPosition) { m_vGrabPosition = vPosition; }
+	
 	bool GetMoveParts() { return m_isMoveParts; }
 	void SetMoveParts(bool b) { m_isMoveParts = b; }
 
