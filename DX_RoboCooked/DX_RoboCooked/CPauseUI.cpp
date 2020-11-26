08fca22c3f7f659a18bec5831a61090e38491264
MyeungJunLee <mong356@naver.com>
2020-11-26 09:46:45 +0900

    no message

--- DX_RoboCooked/DX_RoboCooked/CPauseUI.cpp
+++ DX_RoboCooked/DX_RoboCooked/CPauseUI.cpp
@@ -20,45 +20,45 @@ CPauseUI::~CPauseUI()
 void CPauseUI::Setup()
 {
 
-	////Sprite sp;
-	//
-	//D3DXVECTOR2 vecTemp(0,0);
-	//CUISprite* m_cBody = new CUISprite("UI","panel-info.png",NULL,NULL);
-	//m_cBody->Setup(vecTemp);
-	//
-	//m_cBody->SetParentWorldTM(&m_matWorld);
-	//m_pRoot = m_cBody;
-	////sp.st_Position = D3DXVECTOR2(0, 0); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/panel-info.png");
-	////m_vecSprite.push_back(sp);
-	//m_vSize = g_pUITextureManager->GetTextureSize("UI/panel-info.png");
-	//m_vecBtn.push_back(m_cBody);
-
-	//
-	//vecTemp = D3DXVECTOR2(130, 330);
-	//CUISprite* m_cOk = new CUISprite("UI", "btn-med-over.png", "UI", "btn-med-down.png");
-	//m_cOk->Setup(vecTemp);
-	//m_pRoot->AddChild(m_cOk);
-	//sp.st_Position = D3DXVECTOR2(130, 330); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/btn-med-over.png");
+	Sprite sp;
+	
+	D3DXVECTOR2 vecTemp(0,0);
+	CUISprite* m_cBody = new CUISprite("UI","panel-info.png",NULL,NULL);
+	m_cBody->Setup(vecTemp);
+	
+	m_cBody->SetParentWorldTM(&m_matWorld);
+	m_pRoot = m_cBody;
+	//sp.st_Position = D3DXVECTOR2(0, 0); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/panel-info.png");
 	//m_vecSprite.push_back(sp);
-	//m_vecBtn.push_back(m_cOk);
+	m_vSize = g_pUITextureManager->GetTextureSize("UI/panel-info.png");
+	m_vecBtn.push_back(m_cBody);
 
-	//
-	//vecTemp = D3DXVECTOR2(130, 400);
-	//CUISprite* m_cCancel = new CUISprite("UI", "btn-med-over.png", "UI", "btn-med-down.png");
-	//m_cCancel->Setup(vecTemp);
-	//m_pRoot->AddChild(m_cCancel);
-	//sp.st_Position = D3DXVECTOR2(130, 400); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/btn-med-over.png");
-	//m_vecSprite.push_back(sp);
-	//m_vecBtn.push_back(m_cCancel);
+	
+	vecTemp = D3DXVECTOR2(130, 330);
+	CUISprite* m_cOk = new CUISprite("UI", "btn-med-over.png", "UI", "btn-med-down.png");
+	m_cOk->Setup(vecTemp);
+	m_pRoot->AddChild(m_cOk);
+	sp.st_Position = D3DXVECTOR2(130, 330); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/btn-med-over.png");
+	m_vecSprite.push_back(sp);
+	m_vecBtn.push_back(m_cOk);
+
+	
+	vecTemp = D3DXVECTOR2(130, 400);
+	CUISprite* m_cCancel = new CUISprite("UI", "btn-med-over.png", "UI", "btn-med-down.png");
+	m_cCancel->Setup(vecTemp);
+	m_pRoot->AddChild(m_cCancel);
+	sp.st_Position = D3DXVECTOR2(130, 400); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/btn-med-over.png");
+	m_vecSprite.push_back(sp);
+	m_vecBtn.push_back(m_cCancel);
 
 
-	//vecTemp = D3DXVECTOR2(400, 80);
-	//CUISprite* m_cExit = new CUISprite("UI", "btn-main-menu.png",NULL,NULL);
-	//m_cExit->Setup(vecTemp);
-	//m_pRoot->AddChild(m_cExit);
-	//sp.st_Position = D3DXVECTOR2(400, 80); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/btn-main-menu.png");
-	//m_vecSprite.push_back(sp);
-	//m_vecBtn.push_back(m_cExit);
+	vecTemp = D3DXVECTOR2(400, 80);
+	CUISprite* m_cExit = new CUISprite("UI", "btn-main-menu.png",NULL,NULL);
+	m_cExit->Setup(vecTemp);
+	m_pRoot->AddChild(m_cExit);
+	sp.st_Position = D3DXVECTOR2(400, 80); sp.st_SpriteSize = g_pUITextureManager->GetTextureSize("UI/btn-main-menu.png");
+	m_vecSprite.push_back(sp);
+	m_vecBtn.push_back(m_cExit);
 
 
 }
