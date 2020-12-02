e9a28357894deb9cd6871f3044e9bb7540f9e4b3
3355moijam <3355moijam@gmail.com>
2020-12-02 18:12:42 +0900

    CC 설계중

--- DX_RoboCooked/DX_RoboCooked/CCrowdControl.h
+++ DX_RoboCooked/DX_RoboCooked/CCrowdControl.h
@@ -3,5 +3,13 @@
 class CCrowdControl
 {
 public:
-	
+	virtual ~CCrowdControl();
+
+	virtual string& GetID() = 0;
+	virtual float ReverseRotate() { return 0; }
+	virtual float MultiplySpeed() { return 1.0f; }
+	virtual void Render(D3DXVECTOR3& vPos) = 0;
+	virtual bool IsOverlapable() { return false; }
+	virtual bool IsMovable() { return true; }
+	virtual bool IsProtected() { return false; }
 };
