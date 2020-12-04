8c7f2c76657a2f1c946bb8aefbe473c018bd6bf2
3355moijam <3355moijam@gmail.com>
2020-12-04 15:41:38 +0900

    타일, 필드 정리. 메쉬로 전환 중

--- DX_RoboCooked/DX_RoboCooked/CTile.h
+++ DX_RoboCooked/DX_RoboCooked/CTile.h
@@ -5,22 +5,22 @@ class CTile : public CActor
 {
 public:
 	CTile();
-	~CTile();
+	virtual ~CTile();
 
 
-private:
-	Synthesize(LPDIRECT3DTEXTURE9, m_PlaneTexture, PlaneTexture);
-	Synthesize(ST_CUBE, m_stCube, Cube);
-	Synthesize_Pass_by_Ref(LPD3DXMESH, m_pCubeTile, MeshCubeTile);
-	
+protected:
+	//Synthesize(LPDIRECT3DTEXTURE9, m_PlaneTexture, PlaneTexture);
+	//Synthesize(ST_CUBE, m_stCube, Cube);
+	//Synthesize_Pass_by_Ref(LPD3DXMESH, m_pCubeTile, MeshCubeTile);
+	CStaticMesh* m_pSMesh;
 
 public:
-	void Render() override;
-	void Update() override;
-	void AddEvent(eEvent eEvent);
-	void OnEvent(eEvent eEvent, void* _value) override;
-
-	void AddAcceleration(const D3DXVECTOR3& vAccel) override {}
-	void AddForce(const D3DXVECTOR3& vForce) override {}
+	virtual void Render() override;
+	virtual void Update() override;
+	virtual void AddEvent(eEvent eEvent);
+	virtual void OnEvent(eEvent eEvent, void* _value) override;
+	virtual 
+	virtual void AddAcceleration(const D3DXVECTOR3& vAccel) override {}
+	virtual void AddForce(const D3DXVECTOR3& vForce) override {}
 };
 
