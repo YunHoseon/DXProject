af70aee2052692142c00861192068dd718bb695b
3355moijam <3355moijam@gmail.com>
2020-12-10 10:55:44 +0900

    샌드파일에 픽킹용 콜리전 생성

--- DX_RoboCooked/DX_RoboCooked/CSandpile.cpp
+++ DX_RoboCooked/DX_RoboCooked/CSandpile.cpp
@@ -4,21 +4,30 @@
 #include "IInteractCenter.h"
 
 
-CSandpile::CSandpile(IInteractCenter* InteractCenter, D3DXVECTOR3 vPosition) : m_pCollsionArea(nullptr)
+CSandpile::CSandpile(IInteractCenter* InteractCenter, D3DXVECTOR3 vPosition) : m_pCollisionArea(nullptr)
 {
+	m_fMass = 0;
+	m_fFriction = 0;
+	m_fRepulsivePower = 0;
+	
 	m_pSMesh = g_pStaticMeshManager->GetStaticMesh("Sandpile");
 	m_pInteractCenter = InteractCenter;
-	m_pCollsionArea = new CBoxCollision(D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(1, 1, 1), &m_matWorld);
+	m_pCollision = new CBoxCollision(D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(100, 100, 100), &m_matWorld);
 	SetScale(0.01f, 0.01f, 0.01f);
+	m_pCollision->SetActive(false);
+	
 	SetRotationY(0);
 	SetPosition(vPosition);
 
-	m_pCollsionArea->Update();
+	m_pCollisionArea = new CBoxCollision(D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(0.8, 0.3, 0.8), &m_matWorld);
+	m_pCollisionArea->Update();
 }
 
 
 CSandpile::~CSandpile()
 {
+	SafeDelete(m_pCollisionArea);
+	SafeDelete(m_pCollision);
 }
 
 void CSandpile::Render()
@@ -30,11 +39,27 @@ void CSandpile::Render()
 		m_pSMesh->Render();
 	}
 
-	_DEBUG_COMMENT if (m_pCollsionArea)
-		_DEBUG_COMMENT 	m_pCollsionArea->Render();
+	_DEBUG_COMMENT if (m_pCollision)
+		_DEBUG_COMMENT 	m_pCollision->Render();
+	_DEBUG_COMMENT if (m_pCollisionArea)
+		_DEBUG_COMMENT 	m_pCollisionArea->Render();
 }
 
 void CSandpile::Update()
 {
-	m_pInteractCenter->CheckSandDummyArea(m_pCollsionArea);
+	m_pInteractCenter->CheckSandDummyArea(m_pCollisionArea);
+}
+
+void CSandpile::SetPosition(D3DXVECTOR3 vPosition)
+{
+	CActor::SetPosition(vPosition);
+	if (m_pCollisionArea)
+		m_pCollisionArea->Update();
+}
+
+void CSandpile::SetPosition(float x, float y, float z)
+{
+	CActor::SetPosition(x, y, z);
+	if (m_pCollisionArea)
+		m_pCollisionArea->Update();
 }
--- DX_RoboCooked/DX_RoboCooked/CSandpile.h
+++ DX_RoboCooked/DX_RoboCooked/CSandpile.h
@@ -5,11 +5,14 @@ class CSandpile :
 {
 private:
 	CStaticMesh*		m_pSMesh;
-	ICollisionArea*		m_pCollsionArea;
+	ICollisionArea*		m_pCollisionArea;
 public:
 	CSandpile(IInteractCenter* InteractCenter , D3DXVECTOR3 vPosition);
 	~CSandpile();
 	void Render() override;
 	void Update() override;
+
+	void AddForce(const D3DXVECTOR3& vForce) override {}
+	void AddAcceleration(const D3DXVECTOR3& vAccel) override {}
 };
 
