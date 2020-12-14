f278a60ca845bef5c6cf81a40ab3db73bd5b5c4b
YunHoseon <hosun2776@gmail.com>
2020-12-14 21:13:47 +0900

    no message

--- DX_RoboCooked/DX_RoboCooked/CUILoading.cpp
+++ DX_RoboCooked/DX_RoboCooked/CUILoading.cpp
@@ -1,12 +1,24 @@
 #include "stdafx.h"
 #include "CUILoading.h"
+#include "CUITexture.h"
 
-
-CUILoading::CUILoading()
+CUILoading::CUILoading(D3DXVECTOR2 vPos)
+	: m_isLoading(false)
 {
+	m_vPosition = vPos;
+	g_EventManager->Attach(eEvent::KeyPress, this);
 }
 
 
 CUILoading::~CUILoading()
+{
+}
+
+void CUILoading::Setup()
+{
+	m_pTexture = new CUITexture("data/UI/roadingScreen.png", "data/UI/roadingScreen.png", "data/UI/roadingScreen.png", m_vPosition);
+}
+
+void CUILoading::Render()
 {
 }
