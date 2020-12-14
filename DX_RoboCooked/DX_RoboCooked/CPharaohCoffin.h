aa9481be5bcdaf7ba1edf68e317c13409b049b71
YunHoseon <hosun2776@gmail.com>
2020-12-14 17:05:36 +0900

    관 완성

--- DX_RoboCooked/DX_RoboCooked/CPharaohCoffin.h
+++ DX_RoboCooked/DX_RoboCooked/CPharaohCoffin.h
@@ -4,13 +4,17 @@
 
 #define PARTS_COUNT 5
 class CRandomNumberGenerator;
+class CCharacter;
 
 class CPharaohCoffin :
 	public CInteractiveActor, public IPartGenerator
 {
 	CStaticMesh*	m_pSMesh;
 	bool			m_isInteractCalled;
+	bool			m_isMoveBlocked;
+	float			m_fCoolTime;
 	float			m_fPassedTime;
+	ICollisionArea*		m_pCCCollision;
 
 	string			m_arrPartsID[PARTS_COUNT];	//2´Ü°èÆÄÃ÷ ID
 	CRandomNumberGenerator	m_randNumGenerator;
@@ -23,6 +27,5 @@ public:
 	void Interact(CCharacter* pCharacter) override;
 
 	CParts* Make();
-	void StopMove();
 };
 
